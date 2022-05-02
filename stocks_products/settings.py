ac206101a8d50e388803bc0371fe6747ceab0078
mikhailBrann <alunos13@yandex.com>
2022-05-02 22:00:12 +0300

    change db port

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'stocks_products',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5434',
         'USER': 'castom',
         'PASSWORD': 'castom'
     }
